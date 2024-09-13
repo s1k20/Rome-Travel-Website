9eb5c443702351e9fc61a9a758f91969bdc1d394
Shlok Patel <118121660+s1k20@users.noreply.github.com>
2024-09-13 09:05:13 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// function scroll() {
+//     var container = document.querySelector('.scroll-container');
+//     var scrollAmount = container.scrollWidth - container.clientWidth;
+//     container.scrollTo({
+//       left: scrollAmount,
+//       behavior: 'smooth'
+//     });
+//   }
